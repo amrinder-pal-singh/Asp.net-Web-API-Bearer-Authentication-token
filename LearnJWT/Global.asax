f76d12ab328c0164a689ea0d60db66302eb8b97b
amrinder-pal-singh <amrinderpal.singh@outlook.com>
2017-04-12 02:38:55 +0530

    First Commit to Learn  JWT: First Api and Sample code for Generating Bearer Authentication token

--- LearnJWT/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LearnJWT.WebApiApplication" Language="C#" %>
---
+++ LearnJWT/Startup.cs
@@ -0,0 +1,42 @@
+﻿using LearnJWT.Provider;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(LearnJWT.Startup))]
+namespace LearnJWT
+{
+    
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+            HttpConfiguration config = new HttpConfiguration();
+            ConfigureOAuth(app);
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
